1e587bb7eb458ec06102d488dcebdf3fcd443255
unknown <jackone.action.software@gmail.com>
2023-09-24 10:31:28 -0300

    replacing rare characters, verify the length of each paragraph of each fragment

--- main_02_processing.py
+++ main_02_processing.py
@@ -1,23 +1,42 @@
 import rich
 from rich import print
 import main_01_preprocessing as pre
+import pandas as pd
+from tqdm import tqdm
 
-print(pre.corpus)
+pd.options.plotting.backend = "plotly"
 
-"""
-[Output]
-                                           fragment_01                                        fragment_02  ...                                        fragment_11                                        fragment_12
-0                              I. A SCANDAL IN BOHEMIA                          II. THE RED-HEADED LEAGUE  ...             XI. THE ADVENTURE OF THE BERYL CORONET           XII. THE ADVENTURE OF THE COPPER BEECHES
-1    To Sherlock Holmes she is always _the_ woman. ...  I had called upon my friend, Mr. Sherlock Holm...  ...  “Holmes,” said I as I stood one morning in our...  “To the man who loves art for its own sake,” r...
-2    I had seen little of Holmes lately. My marriag...  “You could not possibly have come at a better ...  ...  My friend rose lazily from his armchair and st...  “And yet,” said I, smiling, “I cannot quite ho...
-3    One night—it was on the twentieth of March, 18...              “I was afraid that you were engaged.”  ...  He was a man of about fifty, tall, portly, and...  “You have erred, perhaps,” he observed, taking...
-4    His manner was not effusive. It seldom was; bu...                           “So I am. Very much so.”  ...  “What on earth can be the matter with him?” I ...  “It seems to me that I have done you full just...
-..                                                 ...                                                ...  ...                                                ...                                                ...
-255                                 “This photograph!”                                               None  ...                                               None                                               None
-256               The King stared at him in amazement.                                               None  ...                                               None                                               None
-257  “Irene’s photograph!” he cried. “Certainly, if...                                               None  ...                                               None                                               None
-258  “I thank your Majesty. Then there is no more t...                                               None  ...                                               None                                               None
-259  And that was how a great scandal threatened to...                                               None  ...                                               None                                               None
+# load DataFrame
+df = pre.corpus
 
-[260 rows x 12 columns]
-"""
+# apply str.replace() to each element of the DataFrame
+df = df.map(lambda x: x.replace('“', '"'), na_action='ignore')
+df = df.map(lambda x: x.replace('”', '"'), na_action='ignore')
+df = df.map(lambda x: x.replace('’', "'"), na_action='ignore')
+
+
+print("Processing Done!")
+
+if __name__ == '__main__':
+    
+    # check length of each element of the DataFrame
+    lengths = {}
+    for column in tqdm(df.columns):
+        lengths[column] = df[column].str.len()
+
+    lengths_df = pd.DataFrame(lengths)
+
+    # print the resulting DataFrame
+
+    parrafo = 0
+    fig = lengths_df.iloc[:, parrafo].plot(kind='bar',
+                                           title='Conteo de longitud de párrafos',
+                                           height=400,
+                                           width=1200,
+                                           template='plotly_dark')
+
+    # save fig
+    # fig.write_html("lengths.html")
+
+    # print(df)
+    print(lengths_df)
