6a17bbf777752dd583598a24a647a28f14aa13ea
unknown <jackone.action.software@gmail.com>
2023-09-24 12:03:49 -0300

    Se agrega bloque de ejecución individualizada

--- main_02_processing.py
+++ main_02_processing.py
@@ -3,6 +3,7 @@ from rich import print
 import main_01_preprocessing as pre
 import pandas as pd
 from tqdm import tqdm
+import os
 
 pd.options.plotting.backend = "plotly"
 
@@ -18,16 +19,23 @@ df = df.map(lambda x: x.replace('’', "'"), na_action='ignore')
 print("Processing Done!")
 
 if __name__ == '__main__':
+    # define the path to the folder
+    folder_path = "visualization"
+
+    # check if the folder exists
+    if not os.path.exists(folder_path):
+        # create the folder if it doesn't exist
+        os.makedirs(folder_path)
     
     # check length of each element of the DataFrame
     lengths = {}
     for column in tqdm(df.columns):
         lengths[column] = df[column].str.len()
-
     lengths_df = pd.DataFrame(lengths)
 
-    # print the resulting DataFrame
 
+
+    ## MANUALLY SELECT PARAGRAPH TO VISUALIZE
     parrafo = 0
     fig = lengths_df.iloc[:, parrafo].plot(kind='bar',
                                            title='Conteo de longitud de párrafos',
@@ -35,8 +43,9 @@ if __name__ == '__main__':
                                            width=1200,
                                            template='plotly_dark')
 
+
     # save fig
-    # fig.write_html("lengths.html")
+    fig.write_html(f"./visualization/lengths_{parrafo +1}.html")
 
     # print(df)
     print(lengths_df)
