ea1c66b27b853c618f8542a8ee66ae3658701595
unknown <jackone.action.software@gmail.com>
2023-09-25 13:04:28 -0300

    Optimization Done!

---
+++ main_03_model.py
@@ -0,0 +1,93 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+import sys
+import main_02_processing as pro
+from tqdm import tqdm
+import threading
+import pickle
+
+
+## path output
+path_out = pro.path_out
+
+
+##  control variables
+fragment_to_translate = 0
+number_of_paragraphs = 50
+
+
+## Load data
+df = pro.df
+model_name = "Helsinki-NLP/opus-mt-en-es"
+
+
+## Check if GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if device == "cpu":
+    print("torch.cuda is not available, exiting...")
+    sys.exit(0)
+
+
+##  Tokenizer and model initialization
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+
+## Function that execute the model
+translated = {}
+def translate(input_ids, posicion):
+    with torch.no_grad():
+        trans = model.generate(**input_ids)
+        translated[posicion] = trans
+
+
+## Thread execution
+if number_of_paragraphs == 0:
+    number_of_paragraphs = df.shape[0]
+batch = df.iloc[:number_of_paragraphs, fragment_to_translate].dropna().to_list()
+threads = []
+print("Tokenization...")
+for i in tqdm(range(len(batch))):
+    tokenized_text = tokenizer(
+        batch[i], padding=True, truncation=True, return_tensors="pt")
+    t = threading.Thread(target=translate, args=(tokenized_text, i))
+    t.start()
+    threads.append(t)
+
+print("Translating...")
+for t in tqdm(threads):
+    t.join()
+
+
+## Decode
+translates = []
+print("Decoding...")
+for i in tqdm(range(len(translated))):
+    trans = tokenizer.decode(translated[i][0], skip_special_tokens=True)
+    translates.append(trans)
+
+
+## show results
+print("Saving results...")
+
+## save results
+save_path = f"{path_out}/translated_fragment_{fragment_to_translate+1}.txt"
+with open(save_path, "w") as f:
+    f.write("\n\n".join(translates))
+    
+print(f"Translated succesfully saved in {save_path}")
+
+
+
+# ### old
+# fragment_encoded = []
+# for i in range(df.shape[1]):
+#     batch = df.iloc[:, i].dropna().to_list()
+
+#     tokenized_text = tokenizer(
+#         batch, padding=True, truncation=True, return_tensors="pt")
+
+#     fragment_encoded.append(tokenized_text)
+
+# print("Tokenization Done!")
+# print(f"fragment_encoded: {len(fragment_encoded)}")
